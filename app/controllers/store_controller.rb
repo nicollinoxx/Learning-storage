2a41f453b1072e2b3d9534f96e57bd3e1a6da3d8
nicollinoxx <nicolasalmeida321@gmail.com>
2023-07-19 19:15:05 -0300

    Add cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
+before_action :count_presence, only: %i[ index ]
+
   def index
     @products = Product.order(:title)
+  end
+
+  private
+
+  def count_presence
+     cookies[:counter] = cookies[:counter].to_i + 1
   end
 end
