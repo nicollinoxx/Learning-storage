04073f468626a7ca73f85911ec7c129f10cc7107
nicollinoxx <nicolasalmeida321@gmail.com>
2023-07-22 17:33:14 -0300

    fixed cart destruction

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -49,15 +49,13 @@ class CartsController < ApplicationController
 
   # DELETE /carts/1 or /carts/1.json
   def destroy
-    @cart.destroy
-    if @cart.id == session[:cart_id]
-        session[:cart_id] = nil
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
 
-      respond_to do |format|
-        format.html { redirect_to store_index_url, notice: 'Your cart is currently empty' }
-        format.json { head :no_content }
-      end
-    end 
+    respond_to do |format|
+      format.html { redirect_to store_index_url, notice: 'Your cart is currently empty' }
+      format.json { head :no_content }
+    end
   end
 
   private
