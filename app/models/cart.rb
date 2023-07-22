123b54530873aaac002c3f8c0f8922d3e775737f
nicollinoxx <nicolasalmeida321@gmail.com>
2023-07-22 17:32:07 -0300

    updated model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,7 +4,6 @@ class Cart < ApplicationRecord
   def add_product(product)
     current_item = line_items.find_by(product_id: product.id)
     if current_item
-      current_item
       current_item.quantity += 1
     else
       current_item = line_items.build(product_id: product.id)
