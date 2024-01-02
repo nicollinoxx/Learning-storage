f7be86c74ab8535348228bd3d7f48f77671d767b
Nicolas Amaral de Almeida <nicolasalmeida321@gmail.com>
2024-01-01 22:25:03 -0300

    Add locale switcher

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,11 +1,15 @@
 class StoreController < ApplicationController
   include CurrentCart
-  
+
   skip_before_action :authorize
   before_action :set_cart
   before_action :count_presence, only: %i[ index ]
 
   def index
+    if params[:set_locale]
+      redirect_to store_index_url(locale: params[:set_locale])
+    end
+
     @products = Product.order(:title)
   end
 
